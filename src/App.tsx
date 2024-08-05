4d7df7b0f386503ca363207747aa668715143ced
surasak chaiyakham <fam.workmotion@gmail.com>
2024-08-05 16:34:53 +0700

    fix config in .circleci

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is circleci by fam
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
